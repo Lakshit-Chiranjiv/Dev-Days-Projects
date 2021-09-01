47fcf2a56ea51770bc63a0febaad0780d8e9f6ac
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2021-09-01 21:31:22 +0530

    project week 1

---
+++ Android Dev/Lengthunitconverter/app/src/main/java/com/example/lengthunitconverter/SplashscreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.lengthunitconverter
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashscreenActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
