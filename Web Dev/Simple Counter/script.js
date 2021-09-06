7951b693d23f10e4cc19d293f09b7f202f33d442
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2021-09-07 00:31:17 +0530

    simple counter

--- Web Dev/Simple Counter/script.js
+++ Web Dev/Simple Counter/script.js
@@ -1,9 +1,11 @@
 let num = document.getElementById("number");
 let plus = document.getElementById("btplus");
 let minus = document.getElementById("btminus");
+let res = document.getElementById("btreset");
 let a;
 plus.addEventListener("click", increase);
 minus.addEventListener("click", decrease);
+res.addEventListener("click",makezero)
 
 function increase() {
   a = Number(num.textContent);
@@ -15,4 +17,8 @@ function decrease() {
   a = Number(num.textContent);
   a--;
   num.innerHTML = `${a}`;
+}
+
+function makezero() {
+  num.innerHTML = `0`;
 }
